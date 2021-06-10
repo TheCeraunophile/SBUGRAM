e1c72d2f28de2468677726d8afaa6667005d97ed
Soheil <shahrabi.soheil@gmail.com>
2021-06-10 19:08:40 +0430

    Posting page controller added

--- Client/Controller/AddPostController.java
+++ Client/Controller/AddPostController.java
@@ -1,4 +1,42 @@
-package Client.View;
+package Client.Controller;
+import Server.Databace.Post;
+import Client.Model.DetailsOfClient;
+import Client.Model.PageLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import java.io.IOException;
 
 public class AddPostController {
+
+    public Button publish;
+    public TextArea textOfPost;
+    public Button cancel;
+
+    public void publish(){
+        if(!textOfPost.getText().equals("")){
+            Post post = new Post(DetailsOfClient.getProfile(), textOfPost.getText());
+            try {
+                DetailsOfClient.oos.writeObject(post);
+                DetailsOfClient.oos.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            jumpToBack();
+        }else {
+            // TODO: 09/06/2021 one popup
+        }
+    }
+
+    private void jumpToBack(){
+        try {
+            new PageLoader().load("TimeLine");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cancel(){
+        jumpToBack();
+    }
+
 }
