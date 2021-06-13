733b8b2d671733b51948eb6a56089d772efa2c5b
Soheil <shahrabi.soheil@gmail.com>
2021-06-13 16:04:51 +0430

    Details of any post updated

--- Client/Controller/PostItemController.java
+++ Client/Controller/PostItemController.java
@@ -21,11 +21,16 @@ public class PostItemController {
 
     public AnchorPane init() {
         username.setText(post.getSender().getUsername());
-        title.setText(post.getText());
-//        String temp = post.getText();
-//        title.setText(temp.substring(0,temp.indexOf("\n")));
-//        description.setText(temp.substring(temp.indexOf("\n"+1)));
-//        profileImage.setImage(new Image(Paths.get("images/ali_alavi.jpg").toUri().toString()));
+        if (post.getText().contains("\n")){
+        String temp = post.getText();
+        title.setText(temp.substring(0,temp.indexOf("\n")));
+        description.setText(temp.substring(temp.indexOf("\n"+1)));
+        }
+        else {
+            title.setText("without any title");
+            description.setText(post.getText());
+        }
+        //        profileImage.setImage(new Image(Paths.get("images/ali_alavi.jpg").toUri().toString()));
         return root;
     }
 }
