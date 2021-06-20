0a15aafe2fb26a70d6edc0d490810f7808b6a117
Soheil <shahrabi.soheil@gmail.com>
2021-06-20 10:45:19 +0430

    One part named user added to show lists of followers following and search table

---
+++ Client/Controller/UserItemController.java
@@ -0,0 +1,2 @@
+package Client.Controller;public class UserItemController {
+}
--- module-info.java
+++
@@ -1,8 +0,0 @@
-module TextGram {
-    requires javafx.graphics;
-    requires javafx.controls;
-    requires javafx.fxml;
-    opens Client.View;
-    opens Client.Model;
-    opens Client.Controller;
-}
